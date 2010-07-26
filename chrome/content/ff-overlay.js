03ce6136f53c6eb301f168492ceb6f24027edf6f
Kosei Moriyama <cou929@gmail.com>
2010-07-27 01:53:58 +0900

    change data model

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -1,11 +1,10 @@
 var atndevrecom = {
   oldUri: null,
-  events: {},
-  sortingArray: [],
   isActive: false,
   monthString: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
   weekString: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
   titleLenght: 30,
+  eventStore: {},
 
   // cache
   today: null,
@@ -73,16 +72,17 @@ var atndevrecom = {
     document.getElementById('atndevrecom-popup-panel').openPopup(document.getElementById('status-bar'), 'before_end', -20, 0, false, false);
   },
 
-  constructPanel: function() {
+  constructPanel: function(originalEventId) {
     atndevrecom.clearPanel();
-    atndevrecom.sortingArray.sort(atndevrecom.cmp);
+    var store = atndevrecom.eventStore[originalEventId];
+    store.sortingArray.sort(atndevrecom.cmp);
 
     var html = '<div id="atndevrecom-results" xmlns="http://www.w3.org/1999/xhtml"><table cellspacing="0" cellpadding="0" border="0">'
       + '<thead><tr class="head"><th class="date-head">Date</th><th class="title-head">Title</th><th class="limit-head">Limit</th></tr></thead>'
       + '<tbody>';
-    for (var i=0; i<atndevrecom.sortingArray.length; i++) {
-      var currentEvent = atndevrecom.events[atndevrecom.sortingArray[i].event_id];
-      var day = atndevrecom.sortingArray[i].date;
+    for (var i=0; i<store.sortingArray.length; i++) {
+      var currentEvent = store.events[store.sortingArray[i].event_id];
+      var day = store.sortingArray[i].date;
       var limit = currentEvent.limit ? currentEvent.limit : 'NA';
       var limitClass = (limit != 'NA' && (currentEvent.accepted + currentEvent.waiting) < limit) ? 'under' : '';
       html += '<tr onclick="atndevrecom.openNewTab(\'' + currentEvent.event_url + '\')">'
@@ -169,6 +169,10 @@ var atndevrecom = {
   getEventList: function(users, originalEventId) {
     var url = "http://api.atnd.org/events/?format=json&count=100&user_id=";
     var userNum = users.length;
+    atndevrecom.eventStore[originalEventId] = {};
+    var store = atndevrecom.eventStore[originalEventId];
+    store.events = {};
+    store.sortingArray = [];
 
     atndevrecom.drawLoading();
 
@@ -181,10 +185,6 @@ var atndevrecom = {
 
     function onload(xhr, user) {
       return function() {
-        if (--userNum == 0) {
-          atndevrecom.activate();
-          atndevrecom.constructPanel();
-        }
         if (xhr.readyState == 4 && xhr.status == 200) {
           var res = JSON.parse(xhr.responseText);
           for (var j=0; j<res.events.length; j++) {
@@ -192,14 +192,18 @@ var atndevrecom = {
             if (atndevrecom.today < startDate && 
                 Math.abs(startDate - atndevrecom.today) < (60 * 60 * 24 * 365 * 1000) &&
                 res.events[j].event_id != originalEventId)
-              if (atndevrecom.events[res.events[j].event_id]) {
-                atndevrecom.events[res.events[j].event_id].count += 1;
+              if (store.events[res.events[j].event_id]) {
+                store.events[res.events[j].event_id].count += 1;
               } else {
-                atndevrecom.events[res.events[j].event_id] = res.events[j];
-                atndevrecom.events[res.events[j].event_id].count = 1;
-                atndevrecom.sortingArray.push({'date': startDate, 'event_id': res.events[j].event_id});
+                store.events[res.events[j].event_id] = res.events[j];
+                store.events[res.events[j].event_id].count = 1;
+                store.sortingArray.push({'date': startDate, 'event_id': res.events[j].event_id});
               }
           }
+        }
+        if (--userNum == 0) {
+          atndevrecom.activate();
+          atndevrecom.constructPanel(originalEventId);
         }
       };
     };
